b02c7526503c316b02956b8aac9df202c2b442d7
ojAsare910 <owusufaith@gmail.com>
2024-10-05 10:23:10 +0000

    resolved application rsakeys

--- src/main/java/com/ojasare/notes/NotesApplication.java
+++ src/main/java/com/ojasare/notes/NotesApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(RsaKeyProperties.class)
 public class NotesApplication {
 
 	public static void main(String[] args) {
